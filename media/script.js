ed9686ccac551d9a8125a23d6aa3e5c383779db6
inventionalt <masiainzot@educacion.navarra.es>
2024-11-21 10:19:05 +0100

    Update script.js

--- media/script.js
+++ media/script.js
@@ -11,7 +11,7 @@ let engine = new FMusic();
 engine.setTrack(0, {
   clef: 'g',
   text: 'Piano',
-  inner: 't:4/4,n:w/6,b,n:h/7,n:h/6'
+  inner: 't:4/4,n:w/6,b,n:h/7,n:h/6,b,n:q/4/d,n:e/5,n:h/4'
 });
 
 engine.paint('staff')
